e0197d0f66327d3e4147329039d41b422ec5b938
LserranoMedrano <50883127+LserranoMedrano@users.noreply.github.com>
2023-05-25 09:09:07 -0600

    Update HibernateUtil.java
    copy-right

--- src/main/java/ni/com/sts/retirosCSSFV/conexion/HibernateUtil.java
+++ src/main/java/ni/com/sts/retirosCSSFV/conexion/HibernateUtil.java
@@ -23,48 +23,5 @@ import java.util.Properties;
  */
 public class HibernateUtil {
 
-    private static Logger logger = Logger.getLogger(HibernateUtil.class);
-    private static SessionFactory sessionFactory;
-
-    public static SessionFactory getSessionFactory() {
-
-        // para windows
-        final String ARCHIVO = "R:\\properties_automatic\\retiros_config.properties";
-        // para linux
-        final String FILELINUX = "\\home\\properties_automatic\\retiros_config.properties";
-
-        if (sessionFactory == null) {
-            try {
-                InputStream lectura = new FileInputStream(ARCHIVO);
-                Properties properties = new Properties();
-                properties.load(lectura);
-                Configuration configuration = new Configuration();
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, properties.getProperty("DATABASE_DRIVER"));
-                settings.put(Environment.URL, properties.getProperty("DATABASE_URL"));
-                settings.put(Environment.USER, properties.getProperty("DATABASE_USERNAME"));
-                settings.put(Environment.PASS, properties.getProperty("DATABASE_PASSWORD"));
-                settings.put(Environment.DIALECT, properties.getProperty("DATABASE_DIALECT"));
-                settings.put(Environment.SHOW_SQL, "false");
-                settings.put(Environment.FORMAT_SQL, "false");
-                settings.put(Environment.POOL_SIZE,"10");
-                configuration.setProperties(settings);
-                configuration.addAnnotatedClass(Participantes.class);
-                configuration.addAnnotatedClass(ParticipantesProcesos.class);
-                configuration.addAnnotatedClass(DocumentacionRetiroData.class);
-                configuration.addAnnotatedClass(Mensajes.class);
-                configuration.addAnnotatedClass(RetirosThread.class);
-                configuration.addAnnotatedClass(Principal.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-    }
+    
 }
