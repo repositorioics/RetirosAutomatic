f4a662df7c0bf04c2bbc3c0e0d48278e24438b02
LserranoMedrano <50883127+LserranoMedrano@users.noreply.github.com>
2023-05-25 09:10:41 -0600

    Update Principal.java
    copy-right

--- src/main/java/ni/com/sts/retirosCSSFV/Principal.java
+++ src/main/java/ni/com/sts/retirosCSSFV/Principal.java
@@ -23,71 +23,9 @@ import java.util.*;
  */
 public class Principal {
 
-    private static  Logger logger = Logger.getLogger(Principal.class);
-    private static  RetirosThread retirosThread = new RetirosThread();
-
-    public static String dateFormat = "dd/MM/yyyy HH:mm:ss";
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
-
+   
 
     public static void main(String[] args) throws ParseException {
 
-        logger.info(" ** Iniciando clase MAIN... ** ");
-        ParticipanteDao dao = new ParticipanteDao();
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        List<Mensajes> objTiempoDb = dao.getCatalogo("CAT_TIEMPO_DESPERTAR_RETIRO_AUTOMATIC",session);
-
-        //todo: Obtengo la Fecha y Hora desde la Base de Datos  **
-        String fechaDeDespertar =  objTiempoDb.get(0).getEs();
-
-        //FORMATO FECHA Y HORA
-        Date fechaDB = ni.com.sts.retirosCSSFV.utils.UtilDate.StringToDate(fechaDeDespertar, "dd/MM/yyyy HH:mm:ss");
-        long timeInMillis = fechaDB.getTime();
-
-        //TIEMPO DE REPETICION
-        Integer repetir = Integer.parseInt(objTiempoDb.get(1).getEs());
-
-        // FORMATO HORA
-        String hora_desde_db = new SimpleDateFormat("HH:mm:ss").format(fechaDB);
-
-        //SEPARAR HORA,MIN,SEG
-        String string = hora_desde_db;
-        String[] parts = string.split(":");
-        String HORA_DB = parts[0];
-        String MINUTE_DB = parts[1];
-        String SECOND_DB = parts[2];
-        DateFormat fechaFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-
-        // HORA DE DESPERTAR DESDE LA BD
-        Date horaDespertar = new Date(timeInMillis);
-        Calendar c = Calendar.getInstance();
-        c.setTime(horaDespertar);
-
-        // SI LA HORA EN BD ES < A LA HORA ACTUAL ESPERA EL TIEMPO DE EJECUCIÓN
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime DBLocalDateTime = UtilDate.asLocalDateTime(fechaDB);
-        boolean isAfter = now.isAfter(DBLocalDateTime);
-
-        if (isAfter) {
-            c.set(Calendar.DAY_OF_YEAR, c.get(Calendar.DAY_OF_YEAR) + 1);
-            c.set(Calendar.HOUR_OF_DAY, Integer.parseInt(HORA_DB));
-            c.set(Calendar.MINUTE, Integer.parseInt(MINUTE_DB));
-            c.set(Calendar.SECOND, Integer.parseInt(SECOND_DB));
-            horaDespertar = c.getTime();
-            logger.info("Se ha agregado un día a la fecha. "+ fechaFormat.format(horaDespertar));
-        }else{
-            horaDespertar=fechaDB;
-            logger.info("Esperando el tiempo para despertar... "+ fechaFormat.format(horaDespertar));
-        }
-
-        // TIEMPO DE REPETICIÓN DESDE LA DB
-        int tiempoRepeticion = repetir;
-        Timer temporizador = new Timer();
-        temporizador.schedule(new TimerTask() {
-            @Override
-            public void run(){
-                retirosThread.run();
-            }
-        },horaDespertar,tiempoRepeticion);
-    }
+      
 }// fin class DAY_OF_MONTH
